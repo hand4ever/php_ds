de584906f85bffc5587023ad231f5182e5aa0ebe
pldream <panlonggl@163.com>
2018-12-12 10:31:01 +0800

    up

---
+++ origin_linklist.php
@@ -0,0 +1,279 @@
+<?php
+
+/**
+ * 节点
+ * Class Node
+ */
+class Node {
+    /**
+     * ElemType
+     *
+     * @var int
+     */
+    public $data;
+    /**
+     * @var Node
+     */
+    public $next;
+}
+
+/**
+ * 初始化操作，建立一个空的线性表 L
+ * @param $L Node
+ * @param $arr array 默认的初始化值
+ */
+function InitList(&$L, $arr=null)
+{
+    $head = new stdClass;
+    $head->next = null;
+    $L = $head;
+
+    $q = $L;
+    if($arr) {
+        foreach($arr as $v) {
+            $node = new Node;
+            $node->data = $v;
+            //头插法
+//            $node->next = $L->next;
+//            $L->next = $node;
+            //尾插法
+            $node->next = null;
+            $q->next = $node;
+            $q = $node;
+        }
+    }
+    print_r($q);
+    print_r($L);
+
+}
+
+/**
+ * 若线性表为空，返回 true，否则返回 false
+ * @param $L Node
+ * @return bool
+ */
+function ListEmpty($L)
+{
+    if ($L->next==null) return true;
+
+    return false;
+}
+
+/**
+ *
+ * 将线性表清空
+ * @param $L
+ */
+function ClearList(&$L)
+{
+    //in php $L = null; //done!
+    $p = $L->next;
+    $q = null;
+    while($p) {
+        $q = &$p->next;
+//        $p = null;
+        $p->data = null;
+        $p = $q;
+    }
+}
+
+/**
+ * 将线性表 L 中第 i 个元素返回给 e
+ * @param $L
+ * @param $i
+ * @param $e
+ * @return int
+ */
+function GetElem($L, $i, &$e)
+{
+    $p = $L->next;
+    $j = 0;
+    while($p && $j < $i) {
+        $p = &$p->next;
+        $j++;
+    }
+    if(!$p || $j > $i) {
+        return -1;// i 位置不存在
+    }
+
+    $e = $p->data;
+
+    return 0;
+}
+
+/**
+ * 在线性表 L 中查找与 e 相等的元素，如果查找成功，则返回元素所在位置以示成功，否则返回 0 表示失败
+ * @param $L
+ * @param $e
+ * @return int
+ */
+function LocateElem($L, $e)
+{
+    $p = $L->next;
+
+    $i = 0;
+    while($p) {
+        if($p->data == $e) {
+            return $i;
+        }
+        ++$i;
+        $p = &$p->next;
+    }
+
+    return -1;
+}
+
+/**
+ * 在线性表 L 的第 i 个位置插入元素 e
+ * @param $L
+ * @param $i
+ * @param $e
+ * @return int 0: sucess 1: fail
+ */
+function ListInsert(&$L, $i, $e)
+{
+    $p = $L;
+    $j = 0;
+    while($p && $j < $i) {
+        $p = $p->next;
+        $j++;
+    }
+    if(!$p || $j > $i) {
+//        exit("not exist position {$i}");
+        return -1;//不存在第 i 个位置
+    }
+    $node = new Node;
+    $node->data = $e;
+
+    $node->next = &$p->next;
+    $p->next = &$node;
+
+    return 0;
+}
+
+/**
+ * 删除线性表 L 中的第 i 个位置的元素，并用 e 返回其值
+ * @param $L
+ * @param $i
+ * @param $e
+ * @return int
+ */
+function ListDelete(&$L, $i, &$e)
+{
+    $p = $L->next;
+    $j = 0;
+    while($p && $j < $i) {
+        $q = $p;
+        $p = &$p->next;
+        ++$j;
+    }
+    if (!$p && $j > $i) {
+        return -1;// i not exists
+    }
+
+    $e = $p->data;
+    $q->next = $p->next;
+
+    return 0;
+}
+
+/**
+ * 返回线性表 L 的元素个数
+ * @param $L
+ * @return int
+ */
+function ListLength($L)
+{
+    if (ListEmpty($L)) {
+        return 0;
+    }
+    $i = 0;
+    $p = $L->next;
+    while($p) {
+        $i++;
+        $p = &$p->next;
+    }
+    return $i;
+}
+/**
+ * 遍历线性表 L 中的每个元素
+ * @param $L
+ * @return string
+ */
+function ListTraverse($L)
+{
+    if (ListEmpty($L)) return "NULL\n";
+    $p = $L->next;
+    while($p) {
+        echo $p->data,PHP_EOL;
+        $p = &$p->next;
+    }
+    return 0;
+}
+
+/**
+ * 将所有在 La 中但不在 Lb 中的数据插入到 La 结尾
+ * @param $La
+ * @param $Lb
+ */
+function union(&$La, $Lb)
+{
+    $La_len = ListLength($La);
+    $Lb_len = ListLength($Lb);
+    for($i=0; $i<$Lb_len; $i++) {
+        $e = null;
+        GetElem($Lb, $i, $e);
+        if(LocateElem($La, $e) == -1) {
+            ListInsert($La, ++$La_len-1, $e);
+        }
+    }
+}
+
+$L = null;
+InitList($L);
+//ListTraverse($L);
+ListInsert($L, 0, 'a');
+ListInsert($L, 1, 'b');
+ListInsert($L, 2, 'c');
+ListInsert($L, 3, 'd');
+ListInsert($L, 4, 'e');
+ListInsert($L, 5, 'f');
+
+ListInsert($L, 2, 'CC');
+
+echo "L ListLength:", ListLength($L),PHP_EOL;
+//ListTraverse($L);
+//
+////GetElem($L, 3, $e);
+//GetElem($L, 5, $e);
+//echo "GetElem 3:", $e;
+//
+//echo PHP_EOL,'e position is:', LocateElem($L, 'e');
+
+//ListDelete($L, 2, $f);
+//echo PHP_EOL, 'delete pos 2:', $f,PHP_EOL;
+ListTraverse($L);
+//ClearList($L);
+
+
+$Lb = null;
+InitList($Lb);
+ListInsert($Lb, 0, 'a');
+ListInsert($Lb, 1, 'b');
+ListInsert($Lb, 2, 'c3');
+ListInsert($Lb, 3, 'd3');
+ListInsert($Lb, 4, 'e3');
+ListInsert($Lb, 5, 'f');
+
+echo "Lb ListLength:", ListLength($Lb),PHP_EOL;
+ListTraverse($Lb);
+
+union($L, $Lb);
+echo "L ListLength:", ListLength($L),PHP_EOL;
+ListTraverse($L);
+
+
+$Lc = null;
+InitList($Lc, [1,2,3,4]);
+echo "Lc ListLength:", ListLength($L),PHP_EOL;
+ListTraverse($Lc);
---
+++ test.php
@@ -0,0 +1,67 @@
+<?php
+$array = range(1, 100000);
+
+function dummy($array) {}
+
+$i = 0;
+$start = microtime(true);
+while($i++ < 100) {
+    dummy($array);
+}
+
+printf("Used %sS\n", microtime(true) - $start);
+
+$b = &$array; //注意这里, 假设我不小心把这个Array引用给了一个变量
+$i = 0;
+$start = microtime(true);
+while($i++ < 100) {
+    dummy($array);
+}
+printf("Used %ss\n", microtime(true) - $start);
+exit;
+/*$bar = 1;
+$baz = 2;
+function foo($var)
+{
+	$var = 3;
+	echo $GLOBALS["baz"],PHP_EOL;
+	$var = $GLOBALS["baz"];
+}
+foo($bar);
+var_dump($bar);
+__halt_compiler();
+exit;
+*/
+class A {
+	public $foo = 1;
+}
+$a = new A;
+$b = $a;
+xdebug_debug_zval('a');
+xdebug_debug_zval('b');
+$b->foo = 2;
+unset($b);
+xdebug_debug_zval('a');
+//xdebug_debug_zval('b');
+echo $a->foo, PHP_EOL;
+
+$c = new A;
+echo gettype($c);
+$d = &$c;
+echo gettype($c);
+xdebug_debug_zval('c');
+xdebug_debug_zval('d');
+$d->foo = 2;
+unset($d);
+xdebug_debug_zval('c');
+
+echo $c->foo,PHP_EOL;
+
+$e = new A;
+
+function foo($obj) {
+	$obj->foo = 2;
+}
+foo($e);
+
+echo $e->foo, PHP_EOL;
